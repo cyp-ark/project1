29cab5ccddac1cb234b739ac2a2a0a0b6ab2b2d6
ChanyoungPark <chanreverse@gmail.com>
2024-12-12 10:23:32 +0900

    Update corpinfo.py

--- corpinfo.py
+++ corpinfo.py
@@ -1,7 +1,12 @@
 import fincorpinfo
+from KDB import KDBAnalysisApp
+from financial_visualization import main
 
 def run():
     fincorpinfo.show_company_info()
+    app = KDBAnalysisApp()
+    app.run()
+    main()
 
 # Streamlit 실행
 if __name__ == "__main__":
