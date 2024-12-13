d3fc4c4a814d9d7cba09ff0c6e064ebfcabc738e
ChanyoungPark <chanreverse@gmail.com>
2024-12-13 16:06:41 +0900

    Update st.py

--- st.py
+++ st.py
@@ -1,11 +1,14 @@
 import os
 import streamlit as st
 import pandas as pd
-import chatbot
-from economy_news import show_economic_trends
-import calendar_app
-import interview_supporter
+
+import economy_news
 import corpinfo
+import interview_supporter
+import chatbot
+import calendar_app
+
+
 
 # 사이드바 네비게이션
 def show_sidebar_navigation():
@@ -34,7 +37,7 @@ def main():
 
     # 현재 활성화된 섹션에 따라 해당 함수 호출
     if st.session_state.get("section", "경제 현황") == "경제 현황":
-        show_economic_trends()    
+        economy_news.show()
     elif st.session_state["section"] == "기업 동향":
         corpinfo.show()
     elif st.session_state["section"] == "면접 질문":
